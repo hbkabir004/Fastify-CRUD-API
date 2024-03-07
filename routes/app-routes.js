52368e699c59e4df2e3a0fe1e6e05744a3d850fe
ABIR <hbkabir004@gmail.com>
2024-03-07 22:43:25 +0600

    [updated] app-route.js

--- routes/app-routes.js
+++ routes/app-routes.js
@@ -1,4 +1,4 @@
-const members = require('../data/members.json');
+let members = require('../data/members.json');
 
 function routes(app, opts, done) {
     app.get('/', (request, reply) => {
@@ -21,35 +21,41 @@ function routes(app, opts, done) {
     });
 
     // Create a new member
-    //Test Command: curl -X POST -H "Content-Type: application/json" -d '{"name": "New Item"}' http://localhost:3000/items
+    //Test Command: curl -X POST -H "Content-Type: application/json" -d '{"email": "newitem@exateks.com"}' http://localhost:5000/members
     app.post('/members', (request, reply) => {
         const newmember = request.body;
-        members.push(newmember);
-        return newmember;
+        const newmemberID = members.map((member) => member.id).length + 1;
+        const newProperties = {
+            id: `${newmemberID}`,
+        };
+
+        const newAddedMember = { ...newProperties, ...newmember }
+        members.push(newAddedMember);
+        return newAddedMember;
     });
 
     // Update an existing member's data
-    //Test Command: curl -X PUT -H "Content-Type: application/json" -d '{"name": "Updated Item"}' http://localhost:5000/items/0
+    //Test Command: curl -X PUT -H "Content-Type: application/json" -d '{"email": "newitem@exateks.com"}' http://localhost:5000/members/2
     app.put('/members/:id', (request, reply) => {
         const id = parseInt(request.params.id);
         const updatedmember = request.body;
-        console.log(`Uptader member=> ${request}`);
         let found = false;
         members = members.map(member => {
             if (parseInt(member.id) === id) {
                 found = true;
+
                 return { ...member, ...updatedmember };
             }
             return member;
         });
         if (!found) {
-            return reply.code(404).send({ message: 'member not found' });
+            return reply.code(404).send({ message: 'Member not Found! :/' });
         }
-        return { message: 'member updated successfully' };
+        return { message: 'Member Updated Successfully :)' };
     });
 
     // Delete an member
-    //Test Command: curl -X DELETE http://localhost:5000/items/0
+    //Test Command: curl -X DELETE http://localhost:5000/members/0
     app.delete('/members/:id', (request, reply) => {
         const id = parseInt(request.params.id);
         const initialLength = members.length;
@@ -57,7 +63,7 @@ function routes(app, opts, done) {
         if (members.length === initialLength) {
             return reply.code(404).send({ message: 'member not found' });
         }
-        return { message: 'member deleted successfully' };
+        return { message: 'Member Deleted Successfully ;)' };
     });
 
     done();
